ed4dbd23a2bc7720802fc669a44f0134a735358a
Isabella Ferrandiz <idferran@syr.edu>
2025-06-26 14:04:14 -0400

    data

--- Isabella/wnba.R
+++ Isabella/wnba.R
@@ -6,40 +6,40 @@ library(tidyverse)
 
 ######        Season Level Stats        ############
 
-url_2022<- "https://www.basketball-reference.com/wnba/years/2022_per_game.html"
-
-wnba_2022<- read_html(url_2022) %>% 
-  html_element("table.stats_table") %>% html_table()
-
-wnba_2022 <- wnba_2022 %>% clean_names() %>% 
-  select(-g_2, -mp_2, -gs, -pf)
-
-
-url_2023 <- "https://www.basketball-reference.com/wnba/years/2023_per_game.html"
-
-wnba_2023<- read_html(url_2023) %>% 
-  html_element("table.stats_table") %>% html_table()
-
-wnba_2023 <- wnba_2023 %>% clean_names() %>% 
-  select(-g_2, -mp_2, -gs, -pf)
-
-
-url_2024 <- "https://www.basketball-reference.com/wnba/years/2022_per_game.html"
-
-wnba_2024<- read_html(url_2024) %>% 
-  html_element("table.stats_table") %>% html_table()
-
-wnba_2024 <- wnba_2024 %>% clean_names() %>% 
-  select(-g_2, -mp_2, -gs, -pf)
-
-
-url_2025<- "https://www.basketball-reference.com/wnba/years/2022_per_game.html"
-
-wnba_2025<- read_html(url_2025) %>% 
-  html_element("table.stats_table") %>% html_table()
-
-wnba_2025 <- wnba_2025 %>% clean_names() %>% 
-  select(-g_2, -mp_2, -gs, -pf)
+# url_2022<- "https://www.basketball-reference.com/wnba/years/2022_per_game.html"
+# 
+# wnba_2022<- read_html(url_2022) %>% 
+#   html_element("table.stats_table") %>% html_table()
+# 
+# wnba_2022 <- wnba_2022 %>% clean_names() %>% 
+#   select(-g_2, -mp_2, -gs, -pf)
+# 
+# 
+# url_2023 <- "https://www.basketball-reference.com/wnba/years/2023_per_game.html"
+# 
+# wnba_2023<- read_html(url_2023) %>% 
+#   html_element("table.stats_table") %>% html_table()
+# 
+# wnba_2023 <- wnba_2023 %>% clean_names() %>% 
+#   select(-g_2, -mp_2, -gs, -pf)
+# 
+# 
+# url_2024 <- "https://www.basketball-reference.com/wnba/years/2022_per_game.html"
+# 
+# wnba_2024<- read_html(url_2024) %>% 
+#   html_element("table.stats_table") %>% html_table()
+# 
+# wnba_2024 <- wnba_2024 %>% clean_names() %>% 
+#   select(-g_2, -mp_2, -gs, -pf)
+# 
+# 
+# url_2025<- "https://www.basketball-reference.com/wnba/years/2022_per_game.html"
+# 
+# wnba_2025<- read_html(url_2025) %>% 
+#   html_element("table.stats_table") %>% html_table()
+# 
+# wnba_2025 <- wnba_2025 %>% clean_names() %>% 
+#   select(-g_2, -mp_2, -gs, -pf)
 
 
 
@@ -83,10 +83,10 @@ a_wnba_2025 <- a_wnba_2025 %>% clean_names() %>%
 
 #########           Adding a Year Column        #########
 
-wnba_2022 <- wnba_2022 %>% mutate(year = 2022)
-wnba_2023 <- wnba_2023 %>% mutate(year = 2023)
-wnba_2024 <- wnba_2024 %>% mutate(year = 2024)
-wnba_2025 <- wnba_2025 %>% mutate(year = 2025)
+# wnba_2022 <- wnba_2022 %>% mutate(year = 2022)
+# wnba_2023 <- wnba_2023 %>% mutate(year = 2023)
+# wnba_2024 <- wnba_2024 %>% mutate(year = 2024)
+# wnba_2025 <- wnba_2025 %>% mutate(year = 2025)
 
 a_wnba_2022 <- a_wnba_2022 %>% mutate(year = 2022)
 a_wnba_2023 <- a_wnba_2023 %>% mutate(year = 2023)
@@ -98,13 +98,53 @@ a_wnba_2025 <- a_wnba_2025 %>% mutate(year = 2025)
 
 all_advanced <- bind_rows( a_wnba_2022, a_wnba_2023, a_wnba_2024, a_wnba_2025)
 
-all_season_stats <- bind_rows( wnba_2022, wnba_2023, wnba_2024, wnba_2025)
+# all_season_stats <- bind_rows( wnba_2022, wnba_2023, wnba_2024, wnba_2025)
 
 #########         Removing Player Rows       ###############
 
 all_advanced <- all_advanced[!all_advanced$player== "Player",]
 
-all_season_stats <- all_season_stats[!all_season_stats$player== "Player",]
+
+# all_season_stats <- all_season_stats[!all_season_stats$player== "Player",]
+
+
+########      Removing player-year multiple rows (trades)       ############
+#                 keeping the TOT of that year
+
+# Step 1: Identify players-years with multiple rows (traded players)
+multi_rows <- all_advanced %>%
+  group_by(player, year) %>%
+  filter(n() > 1) %>%          # more than one row for that player-year
+  ungroup() %>%
+  select(player, year) %>%
+  distinct()
+
+# Step 2: Find last team per player-year (excluding TOT)
+last_team_per_player_year <- all_advanced %>%
+  filter(team != "TOT") %>%
+  semi_join(multi_rows, by = c("player", "year")) %>%  # only for multi-row player-years
+  group_by(player, year) %>%
+  slice_tail(n = 1) %>%
+  select(player, year, last_team = team)
+
+# Step 3: Update TOT rows for these multi-row player-years
+tot_rows_updated <- all_advanced %>%
+  filter(team == "TOT") %>%
+  inner_join(multi_rows, by = c("player", "year")) %>%  # only TOT rows for multi-row players
+  left_join(last_team_per_player_year, by = c("player", "year")) %>%
+  mutate(team = last_team) %>%
+  select(-last_team)
+
+# Step 4: For multi-row player-years, keep only the updated TOT rows (drop single teams)
+multi_rows_cleaned <- tot_rows_updated
+
+# Step 5: For player-years with only 1 row or those not traded, keep as is
+single_or_no_trade <- all_advanced %>%
+  anti_join(multi_rows, by = c("player", "year"))
+
+# Step 6: Combine everything
+all_advanced <- bind_rows(single_or_no_trade, multi_rows_cleaned) %>%
+  arrange(player, year)
 
 
 # page <- read_html("https://www.basketball-reference.com/wnba/years/2022.html")
@@ -229,6 +269,8 @@ t_per_game <- bind_rows( t_wnba_2022, t_wnba_2023, t_wnba_2024, t_wnba_2025)
 t_all_advanced <- t_all_advanced[!t_all_advanced$team== "League Average",]
 
 t_per_game <- t_per_game[!t_per_game$team== "League Average",]
+
+
 
 #########         Salary Data         ############
 
